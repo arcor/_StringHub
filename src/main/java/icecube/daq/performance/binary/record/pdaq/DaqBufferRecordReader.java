10b9e3f1fc37b7c1bd287c0e4a66f71a0c7439ce
Tim Bendfelt <tim.bendfelt@icecube.wisc.edu>
2022-09-27 22:12:32 +0000

    Issue 8865: Add describe function and named fields.

--- src/main/java/icecube/daq/performance/binary/record/pdaq/DaqBufferRecordReader.java
+++ src/main/java/icecube/daq/performance/binary/record/pdaq/DaqBufferRecordReader.java
@@ -1,6 +1,9 @@
 package icecube.daq.performance.binary.record.pdaq;
 
+import icecube.daq.cli.commands.RecordCmd;
+import icecube.daq.cli.options.RecordTypeOption;
 import icecube.daq.performance.binary.buffer.RecordBuffer;
+import icecube.daq.performance.binary.record.RecordReader;
 import icecube.daq.performance.binary.record.UTCRecordReader;
 
 import java.nio.ByteBuffer;
@@ -8,20 +11,34 @@ import java.nio.ByteBuffer;
 /**
  * A base definition of DAQ Buffer records.
  *
- * ----------------------------------------------------------------------
- * | length [uint4] |  type (uint4)  |         mbid[uint8]              |
- * ----------------------------------------------------------------------
- * |          padding [unit8]        |         utc[uint8]               |
- * ----------------------------------------------------------------------
- * |              ...                                                   |
- * ----------------------------------------------------------------------
- *
+ * --------------------------------------------------------------------------------------------
+ * | length (uint32)      | type (uint32)        |        mbid (uint64)                       |
+ * --------------------------------------------------------------------------------------------
+ * |               padding (uint64)              |        utc (uint64)                        |
+ * --------------------------------------------------------------------------------------------
+ * |                                payload (byte[length-32]) ...                             |
+ * --------------------------------------------------------------------------------------------
  *
  *
  */
 public class DaqBufferRecordReader extends TypeCodeRecordReader
         implements UTCRecordReader
 {
+
+    private static final String DOC =
+        "--------------------------------------------------------------------------------------------\n" +
+        "| length (uint32)      | type (uint32)        |        mbid (uint64)                       |\n" +
+        "--------------------------------------------------------------------------------------------\n" +
+        "|               padding (uint64)              |        utc (uint64)                        |\n" +
+        "--------------------------------------------------------------------------------------------\n" +
+        "|                                payload (byte[length-32]) ...                             |\n" +
+        "--------------------------------------------------------------------------------------------\n" +
+        "type 2   : Engineering Hit\n" +
+        "type 3   : Delta Compressed Hit\n" +
+        "type 102 : Monitoring Record\n" +
+        "type 202 : Time Calibration Record\n" +
+        "type 302 : Supernova Record\n";
+
 
     public static final DaqBufferRecordReader instance =
             new DaqBufferRecordReader();
@@ -79,4 +96,53 @@ public class DaqBufferRecordReader extends TypeCodeRecordReader
         return (getLength(buffer) == 32 && getUTC(buffer) == Long.MAX_VALUE);
     }
 
+
+    /**
+     * encapsulate fields for generic use
+     */
+    public static final RecordReader.LongField MBID_FIELD = new RecordReader.LongField(){
+        @Override
+        public long value(ByteBuffer buffer, int offset)
+        {
+            return instance.getDOMID(buffer, offset);
+        }
+
+        @Override
+        public long value(RecordBuffer buffer, int offset)
+        {
+            return instance.getDOMID(buffer, offset);
+        }
+    };
+
+    public static final RecordReader.LongField UTC_FIELD = new RecordReader.LongField(){
+        @Override
+        public long value(ByteBuffer buffer, int offset)
+        {
+            return instance.getUTC(buffer, offset);
+        }
+
+        @Override
+        public long value(RecordBuffer buffer, int offset)
+        {
+            return instance.getUTC(buffer, offset);
+        }
+    };
+
+    @Override
+    public LongField getOrderingField()
+    {
+        return UTC_FIELD;
+    }
+
+    @Override
+    public LongField getMbidField()
+    {
+        return MBID_FIELD;
+    }
+
+    @Override
+    public String describe()
+    {
+        return DOC;
+    }
 }
