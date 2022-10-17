0a9ad73edfe4b834ad20980b00d08c0e108f5fe7
Tim Bendfelt <tim.bendfelt@icecube.wisc.edu>
2022-10-17 19:12:40 +0000

    Issue 8865: Add spool name support to spool option.

--- src/main/java/icecube/daq/cli/options/SpoolOption.java
+++ src/main/java/icecube/daq/cli/options/SpoolOption.java
@@ -1,16 +1,22 @@
 package icecube.daq.cli.options;
 
+import icecube.daq.performance.binary.buffer.IndexFactory;
+import icecube.daq.performance.binary.record.RecordReader;
+import icecube.daq.spool.RecordSpool;
 import picocli.CommandLine;
+
+import java.io.File;
+import java.io.IOException;
 
 /**
  * Command line options for specifying a record spool configuration.
  *
  * E.G.
- * --spool hitspool
- * --spool tcalspool:27d
- * --spool tcalspool:8h30m
- * --spool monispool:1024:150000000000:
- * --spool monispool:1024:15s:
+ * --spool spooldir:spoolname
+ * --spool spooldir:hits:27d
+ * --spool spooldir:hits:8h30m
+ * --spool spooldir:hits:1024:150000000000:
+ * --spool spooldir:hits:1024:15s:
  */
 public class SpoolOption
 {
@@ -19,17 +25,31 @@ public class SpoolOption
     public static int DEFAULT_FILE_NUM = 1920;             // 8 hours at 15 second files
 
     public final String spoolDir;
+    public final String spoolName;
     public final int numFiles;
     public final long fileInterval;
 
-    public SpoolOption(String spoolDir, int numFiles, long fileInterval)
+    public SpoolOption(String spoolDir, String spoolName, int numFiles, long fileInterval)
     {
         this.spoolDir = spoolDir;
+        this.spoolName = spoolName;
         this.numFiles = numFiles;
         this.fileInterval = fileInterval;
+
     }
 
-    public static class SpoolOptionsConverter implements CommandLine.ITypeConverter<SpoolOption>
+    public RecordSpool plumbSpool(RecordReader rr) throws IOException
+    {
+        // spool files
+        IndexFactory index = IndexFactory.NO_INDEX;
+        RecordSpool spool = new RecordSpool(rr, rr.getOrderingField(), new File(spoolDir), spoolName,
+                fileInterval, numFiles, index);
+
+        return spool;
+
+    }
+
+    public static class Converter implements CommandLine.ITypeConverter<SpoolOption>
     {
         TimeOption.DurationParameterConverter durationConverter = new TimeOption.DurationParameterConverter();
 
@@ -38,40 +58,50 @@ public class SpoolOption
         {
             String[] tokens = s!=null ? s.split(":") : new String[]{};
 
-            // reasonable restrictions on spool name
-            if(tokens.length > 0)
+            // reasonable restrictions on spool directory and name
+            if(tokens.length > 1)
             {
-                String spoolname = tokens[0];
+                String spooldir = tokens[0];
+                if (spooldir == null ||
+                        spooldir.equals("") ||
+                        spooldir.matches("\\s*"))
+                {
+                    throw new CommandLine.TypeConversionException("Cannot convert " + s + " to a spool configuration," +
+                            "the spool dir [" + spooldir + "] is not permitted");
+
+                }
+
+                String spoolname = tokens[1];
                 if (spoolname == null ||
                         spoolname.equals("") ||
                         spoolname.matches("\\s*"))
                 {
                     throw new CommandLine.TypeConversionException("Cannot convert " + s + " to a spool configuration," +
-                            "the spoolname [" + spoolname + "] is not permitted");
+                            "the spool name [" + spooldir + "] is not permitted");
 
                 }
             }
 
             try {
                 switch (tokens.length) {
-                    case 1:
-                        //use spoolname and default the rest
-                        return new SpoolOption(tokens[0], DEFAULT_FILE_NUM, DEFAULT_INTERVAL);
                     case 2:
-                        //use spoolname and default the second parameter as a target for the overall
+                        //use spool dir and name and default the rest
+                        return new SpoolOption(tokens[0], tokens[1], DEFAULT_FILE_NUM, DEFAULT_INTERVAL);
+                    case 3:
+                        //use spool dir and default the second parameter as a target for the overall
                         // timespan DEFAULT_INTERVAL
-                        TimeOption.TimeDuration duration = durationConverter.convert(tokens[1]);
+                        TimeOption.TimeDuration duration = durationConverter.convert(tokens[2]);
                         int numFiles = (int) ((duration.tenth_nanos + DEFAULT_INTERVAL - 1)/DEFAULT_INTERVAL);
 
-                        return new SpoolOption(tokens[0], numFiles, DEFAULT_INTERVAL);
+                        return new SpoolOption(tokens[0], tokens[1], numFiles, DEFAULT_INTERVAL);
 
-                    case 3:
-                        //use spoolname and default the second parameter as a target for the overall
+                    case 4:
+                        //use spool dir and default the second parameter as a target for the overall
                         // timespan DEFAULT_INTERVAL
-                        numFiles = Integer.parseInt(tokens[1]);
-                        TimeOption.TimeDuration interval = durationConverter.convert(tokens[2]);
+                        numFiles = Integer.parseInt(tokens[2]);
+                        TimeOption.TimeDuration interval = durationConverter.convert(tokens[3]);
 
-                        return new SpoolOption(tokens[0], numFiles, interval.tenth_nanos);
+                        return new SpoolOption(tokens[0], tokens[1], numFiles, interval.tenth_nanos);
 
                     default:
                         throw new CommandLine.TypeConversionException("Cannot convert " + s + " to a spool configuration");
--- src/test/java/icecube/daq/cli/options/SpoolOptionTest.java
+++ src/test/java/icecube/daq/cli/options/SpoolOptionTest.java
@@ -1,7 +1,6 @@
 package icecube.daq.cli.options;
 
 
-import icecube.daq.cli.options.SpoolOption.*;
 import org.junit.Test;
 import picocli.CommandLine;
 
@@ -13,20 +12,22 @@ public class SpoolOptionTest
     @Test
     public void testSpoolOptionConverter()
     {
-        SpoolOptionsConverter subject = new SpoolOptionsConverter();
+        SpoolOption.Converter subject = new SpoolOption.Converter();
 
 
         class Case
         {
             final String input;
             final String expectedSpoolDir;
+            final String expectedSpoolName;
             final int expectedNumFiles;
             final long expectedFileInterval;
 
-            Case(String input, String expectedSpoolDir, int expectedNumFiles, long expectedFileInterval)
+            Case(String input, String expectedSpoolDir, String expectedSpoolName, int expectedNumFiles, long expectedFileInterval)
             {
                 this.input = input;
                 this.expectedSpoolDir = expectedSpoolDir;
+                this.expectedSpoolName = expectedSpoolName;
                 this.expectedNumFiles = expectedNumFiles;
                 this.expectedFileInterval = expectedFileInterval;
             }
@@ -34,18 +35,19 @@ public class SpoolOptionTest
 
         Case[] cases = new Case[]
                 {
-                        new Case("myspool", "myspool", SpoolOption.DEFAULT_FILE_NUM, SpoolOption.DEFAULT_INTERVAL),
-                        new Case("myspool:", "myspool", SpoolOption.DEFAULT_FILE_NUM, SpoolOption.DEFAULT_INTERVAL),
-                        new Case("myspool::", "myspool", SpoolOption.DEFAULT_FILE_NUM, SpoolOption.DEFAULT_INTERVAL),
-                        new Case("myspool:4h", "myspool", 960, SpoolOption.DEFAULT_INTERVAL),
-                        new Case("foo:337:6s", "foo", 337, 60000000000L),
-                        new Case("/var/data/bar:16s", "/var/data/bar", 2, SpoolOption.DEFAULT_INTERVAL),
+                        new Case(".:myspool", ".", "myspool", SpoolOption.DEFAULT_FILE_NUM, SpoolOption.DEFAULT_INTERVAL),
+                        new Case(".:myspool:", ".", "myspool", SpoolOption.DEFAULT_FILE_NUM, SpoolOption.DEFAULT_INTERVAL),
+                        new Case(".:myspool::", ".", "myspool", SpoolOption.DEFAULT_FILE_NUM, SpoolOption.DEFAULT_INTERVAL),
+                        new Case(".:myspool:4h", ".", "myspool", 960, SpoolOption.DEFAULT_INTERVAL),
+                        new Case(".:foo:337:6s", ".", "foo", 337, 60000000000L),
+                        new Case("/var/data/:bar:16s", "/var/data/", "bar", 2, SpoolOption.DEFAULT_INTERVAL),
                 };
 
         for(Case c: cases)
         {
             SpoolOption out = subject.convert(c.input);
             assertEquals(c.expectedSpoolDir, out.spoolDir);
+            assertEquals(c.expectedSpoolName, out.spoolName);
             assertEquals(c.expectedNumFiles, out.numFiles);
             assertEquals(c.expectedFileInterval, out.fileInterval);
         }
@@ -57,21 +59,22 @@ public class SpoolOptionTest
                 {
                         null,
                         "",
-                        " ",
-                        "    ",
-                        ":5h",
-                        "hitspool: ",
-                        "hitspool: : ",
-                        "hitspool:44typo:9999999:",
-                        "hitspool:44:9999999typo:",
+                        ":",
+                        " : ",
+                        "   :  ",
+                        "::5h",
+                        ".:hitspool: ",
+                        ".:hitspool: : ",
+                        ".:hitspool:44typo:9999999:",
+                        ".:hitspool:44:9999999typo:",
 
                 };
         for (String bad : baddies) {
             try
             {
                 SpoolOption out = subject.convert(bad);
-                fail(String.format("String [%s] should have been rejected but parsed to [[%s], [%d], [%d]",
-                        bad, out.spoolDir, out.numFiles, out.fileInterval));
+                fail(String.format("String [%s] should have been rejected but parsed to [[%s], [%s], [%d], [%d]",
+                        bad, out.spoolDir, out.spoolName, out.numFiles, out.fileInterval));
             }
             catch (CommandLine.TypeConversionException tce)
             {
